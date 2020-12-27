127183961dded2e23ef3e32d960a8bddf8e615b4
Kirabium <virgile.fantauzzi@hotmail.fr>
2020-12-27 14:18:23 +0100

    first commit

---
+++ app/src/test/java/com/unittest/mailchecker/CheckMailUnitTest.java
@@ -0,0 +1,18 @@
+package com.unittest.mailchecker;
+
+import org.junit.Test;
+
+import static com.unittest.mailchecker.controller.MainActivity.checkMail;
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class CheckMailUnitTest {
+    @Test
+    public void mail_isCorrect() {
+        assertTrue(checkMail("toto@openclassrooms.fr"));
+    }
+}
